f30fcb2f341ac3f7b9e61a74d40d76e8c4d9abf6
chandan kumar maurya <code.us.cm@gmail.com>
2024-06-21 13:10:28 +0530

    Create Ui Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./Pages/Home";
 
 export default function App() {
   return (
-    <div className='bg-red-500'>App</div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
