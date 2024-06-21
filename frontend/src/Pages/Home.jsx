52c192cd188c8c1dce12d33c05d8ad66aa355acd
chandan kumar maurya <code.us.cm@gmail.com>
2024-06-21 18:25:45 +0530

    Donation Page Ui

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Slider from '../components/Home/Slider'
+import Donation from '@/components/Home/Donation'
 
 export default function Home() {
   return (
     <div>
       <Slider/>
+      <Donation/>
     </div>
   )
 }
