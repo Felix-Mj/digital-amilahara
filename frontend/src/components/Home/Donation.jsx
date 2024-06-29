c57fc2005fdca368ac21105a0cc554b2c6152910
chandan kumar maurya <code.us.cm@gmail.com>
2024-06-29 23:05:15 +0530

    Add footer page and ui

--- frontend/src/components/Home/Donation.jsx
+++ frontend/src/components/Home/Donation.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { Input } from "../ui/input";
 
 export default function Donation() {
+  const {currentUser} = useSelector((state)=>state.user)
+  console.log(currentUser)
+  const [amount, setAmount]= useState()
+  const[useData, setUserData]=useState()
     const handleDonate = () => {
+      const payment = axios.post("/api/v2/payment",
+
+      )
        
     }
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 bg-[#f5f5f5]">
       <div className="container px-4 md:px-6 text-center">
-        <div className="max-w-[600px] mx-auto space-y-4">
+        <div className="flex items-center justify-center flex-col max-w-[600px] mx-auto space-y-4">
           <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
             Support Our Cause
           </h2>
@@ -17,6 +27,14 @@ export default function Donation() {
             need. Help us continue our mission to provide aid and support to the
             community.
           </p>
+          <Input
+          type="number"
+            placeholder="Enter your donation amount"
+            value={amount}
+            className="flex items-center justify-center w-[50%]"
+            onChange={(e) => setAmount(e.target.value)}
+            Value={amount}
+          />
           <Link
           onClick={handleDonate}
             href="#"
