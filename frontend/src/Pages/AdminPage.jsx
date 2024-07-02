1b9ce06eb410765cfc768febc654818a672f19c5
chandan kumar maurya <code.us.cm@gmail.com>
2024-07-02 23:32:18 +0530

    update admin page

--- frontend/src/Pages/AdminPage.jsx
+++ frontend/src/Pages/AdminPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -11,15 +11,34 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "@/components/ui/table";
+import axios from "axios";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 export const AdminPage = () => {
+  const [blogData, setBlogdata] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get("/api/v1/bloglist");
+      setBlogdata(res.data.data);
+    };
+    getData();
+  }, []);
+
   return (
     <div className="flex gap-2 p-2">
       <div className="w-full">
         <Tabs defaultValue="account" className="w-full flex">
           <TabsList className="w-1/5 flex-col mt-20 bg-white">
             <h1 className="text-2xl text-black dark:text-white">Admin Page</h1>
-            <TabsTrigger value="Dashboard" className="mt-5">
+            <TabsTrigger value="Dashboard" className="mt-5 border-none" defaultValue="Dashboard">
               Dashboard
             </TabsTrigger>
             <TabsTrigger value="Event">Slider Event</TabsTrigger>
@@ -30,45 +49,101 @@ export const AdminPage = () => {
             <TabsContent value="Dashboard">
               <Card>
                 <div className="flex">
-                <Card className='w-full'>
-                  <h1 className="flex items-center justify-center">Blog Details</h1>
-                  <table >
-                    <tr>
-                      <th>Title</th>
-                      <th>Delete</th>
-                    </tr>
-                    <tr>
-                      <td>Title</td>
-                      <td>Delete</td>
-                    </tr>
-                  </table>
-                </Card>
-                <Card className='w-full'>
-                  <h1 className="flex items-center justify-center">News Details</h1>
-                  <table>
-                    <tr>
-                      <th>Title</th>
-                      <th>Delete</th>
-                    </tr>
-                    <tr>
-                      <td>Title</td>
-                      <td>Delete</td>
-                    </tr>
-                  </table>
-                </Card>
+                  <Card className="w-full">
+                    <h1 className="flex items-center justify-center font-bold">
+                      Blog Details
+                    </h1>
+                    <div className=" rounded-lg w-full">
+                      <div className="relative w-full overflow-auto">
+                        <Table className="border-none">
+                          <TableHeader>
+                            <TableRow>
+                              <TableHead className="w-[80%]">Title</TableHead>
+                              <TableHead>Category</TableHead>
+                              <TableHead className="text-right">
+                                Delete
+                              </TableHead>
+                            </TableRow>
+                          </TableHeader>
+                          <TableBody>
+                            {blogData.map((e, i) => {
+                              return (
+                                <TableRow key={i}>
+                                  <TableCell className="font-medium">
+                                    {e?.title}
+                                  </TableCell>
+                                  <TableCell>{e?.category}</TableCell>
+                                  <TableCell className="text-right">
+                                    <Button variant="destructive">
+                                      Delete
+                                    </Button>
+                                  </TableCell>
+                                </TableRow>
+                              );
+                            })}
+                          </TableBody>
+                        </Table>
+                      </div>
+                    </div>
+                  </Card>
+                  <Card className="w-full">
+                    <h1 className="flex items-center justify-center font-bold">
+                      News Details
+                    </h1>
+                    <Table className="border-none">
+                      <TableHeader>
+                        <TableRow>
+                          <TableHead className="w-[80%]">Title</TableHead>
+                          <TableHead>Category</TableHead>
+                          <TableHead className="text-right">Delete</TableHead>
+                        </TableRow>
+                      </TableHeader>
+                      <TableBody>
+                        {blogData.map((e, i) => {
+                          return (
+                            <TableRow key={i}>
+                              <TableCell className="font-medium">
+                                {e?.title}
+                              </TableCell>
+                              <TableCell>{e?.category}</TableCell>
+                              <TableCell className="text-right">
+                                <Button variant="destructive">Delete</Button>
+                              </TableCell>
+                            </TableRow>
+                          );
+                        })}
+                      </TableBody>
+                    </Table>
+                  </Card>
                 </div>
-                <Card className='w-full mt-2'>
-                  <h1 className="flex items-center justify-center">Event Details</h1>
-                  <table>
-                    <tr>
-                      <th>Title</th>
-                      <th>Delete</th>
-                    </tr>
-                    <tr>
-                      <td>Title</td>
-                      <td>Delete</td>
-                    </tr>
-                  </table>
+                <Card className="w-full mt-2">
+                  <h1 className="flex items-center justify-center font-bold">
+                    Event Details
+                  </h1>
+                  <Table className="border-none">
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead className="w-[80%]">Title</TableHead>
+                        <TableHead>Category</TableHead>
+                        <TableHead className="text-right">Delete</TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      {blogData.map((e, i) => {
+                        return (
+                          <TableRow key={i}>
+                            <TableCell className="font-medium">
+                              {e?.title}
+                            </TableCell>
+                            <TableCell>{e?.category}</TableCell>
+                            <TableCell className="text-right">
+                              <Button variant="destructive">Delete</Button>
+                            </TableCell>
+                          </TableRow>
+                        );
+                      })}
+                    </TableBody>
+                  </Table>
                 </Card>
               </Card>
             </TabsContent>
