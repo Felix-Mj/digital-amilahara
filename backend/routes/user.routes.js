d9b04e94f326d12b66deeeb758551ef657ec7d90
chandan kumar maurya <code.us.cm@gmail.com>
2024-07-03 16:12:09 +0530

    Create post route and get route contectUs

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { login, logout, signup, userUpdate } from '../controller/user.controller.js';
+import { getContectus, login, logout, postContectus, signup, userUpdate } from '../controller/user.controller.js';
 const route =express.Router()
 
 route.post("/signup",signup)
 route.post("/login",login)
 route.post("/userupdate/:id",userUpdate)
 route.get("/logout",logout)
+route.post("/contectus",postContectus)
+route.get("/contectus",getContectus)
 
 export default route
